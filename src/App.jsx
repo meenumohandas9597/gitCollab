a84970e045839d583066ad83b2b86dc5c557e318
meenumohandas9597 <meenumohandas2004@gmail.com>
2025-10-27 21:29:31 +0530

    sailo started

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import Header from './Pages/Header'
 import View from './Pages/View'
 
 function App() {
-   <Header />
   return (
     <>
-     <h1>Welcome</h1>
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro alias animi quod fugiat distinctio facere sed officiis suscipit. Repudiandae quisquam voluptate rerum aliquam? Animi sit dolores corrupti sapiente commodi quidem.</p>
-     <View/>
-     <Footer/>
+    
     </>
 
     
