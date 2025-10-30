31f8e7fc73a4b057c59c76551108fdb0f399495b
aparnamathew2002 <aparnamathew2002@gmail.com>
2025-10-30 13:39:27 +0530

    about created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,7 +62,7 @@ function Header() {
 
         <ul className="hidden md:flex gap-20 font-semibold text-[15px]">
           <Link to='/'><li className="text-[#4ed4d2] cursor-pointer">Home</li></Link>
-          <Link to='/'><li className="hover:text-[#4ed4d2] cursor-pointer">About</li></Link>
+          <Link to='/about'><li className="hover:text-[#4ed4d2] cursor-pointer">About</li></Link>
           <Link to='/destination'><li className="hover:text-[#4ed4d2] cursor-pointer">Destinations</li></Link>
           <Link to='/tour'><li className="hover:text-[#4ed4d2] cursor-pointer">Tours</li></Link>
           <Link to='/blog'><li className="hover:text-[#4ed4d2] cursor-pointer">Blog</li></Link>
