1997e11afc2fe04e6f96b0f053be2c4f2c684844
meenumohandas9597 <meenumohandas2004@gmail.com>
2025-11-05 22:21:59 +0530

    server modified

--- src/service/serverURL.js
+++ src/service/serverURL.js
@@ -1,3 +1,3 @@
 // export const SERVER_URL = "http://localhost:3000";
 
-export const SERVER_URL = "https://sailo-backend.onrender.com"
+export const SERVER_URL = "https://sailo-backend-1.onrender.com"
